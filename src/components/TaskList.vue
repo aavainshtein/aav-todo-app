3cbcd0fc7953ba1665acef366650be84c9c33d50
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-24 14:19:23 +0200

    feat: add TaskList component for displaying tasks with empty state

---
+++ src/components/TaskList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import TaskItem from './TaskItem.vue'
+
+const store = useStore()
+const tasks = computed(() => store.getters.filteredTasks)
+</script>
+<template>
+  <ul class="task-list">
+    <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
+    <li v-if="!tasks.length" class="empty">Нет задач</li>
+  </ul>
+</template>
+
+<style scoped>
+.task-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.empty {
+  color: #888;
+  text-align: center;
+  padding: 1rem 0;
+}
+</style>
